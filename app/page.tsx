5c08f2aa11dfc232c93f3e66b28580997bb2a13a
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-23 22:44:06 -0300

    add project component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "./_components/Hero";
-import { FaHome } from "react-icons/fa";
+
 import Grid from "./_components/Grid";
+import Projects from "./_components/Projects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid />
+        {/* <Grid /> */}
+        <Projects />
       </div>
     </main>
   );
