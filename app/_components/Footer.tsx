9e94e4c560c34fdf99133abaeac272c627a9267a
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-26 20:55:57 -0300

    add social media and contact info

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,61 @@
+"use client";
+import MagicButton from "@/components/ui/MagicButton";
+import { socialMedia } from "@/data";
+import React, { useState } from "react";
+import { FaLocationArrow } from "react-icons/fa";
+
+const Footer = () => {
+  const [copied, setCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText("tobias.ariel.santellan@gmail.com");
+    setCopied(true);
+  };
+  return (
+    <div className="w-full pt-10 pb-10 " id="contact">
+      <div className="w-full left-0 absolute -bottom-72 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50"
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          I&apos;m <span className="text-green">available</span>. Do{" "}
+          <span className="text-green">you</span> want to work together?
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach me out today
+        </p>
+        <a href="mailto:tobias.ariel.santellan@gmail.com">
+          <MagicButton
+            title={copied ? "Email Copied" : "Let's get in touch"}
+            icon={<FaLocationArrow />}
+            position="right"
+            otherClasses="gap-3"
+            handleClick={handleCopy}
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Tobias Santellan
+        </p>
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((item) => (
+            <div
+              key={item.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center rounded-lg backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-green
+              "
+            >
+              <img src={item.img} alt={item.img} width={20} height={20} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
