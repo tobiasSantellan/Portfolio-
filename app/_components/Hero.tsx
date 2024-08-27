e9c51e0e4ca420cef7c71d7533e43eff477be9ce
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-26 21:53:03 -0300

    changed m-y on mobile

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
-      <div className="flex justify-center relative my-20 z-10">
+      <div className="flex justify-center relative my-5 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-md text-center text-blue-100 max-w-80">
             I am Tobias Santellan
